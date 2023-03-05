4ea4d9709fa118efd26fce3dc83930ff94725dc7
inspirezuza <102022496+inspirezuza@users.noreply.github.com>
2023-03-05 14:28:29 +0700

    Update config.h

--- esp32/sketchpiano/config.h
+++ esp32/sketchpiano/config.h
@@ -1,6 +1,6 @@
-#define WIFI_SSID    "KULOSE"
-#define WIFI_PASS    "12341234"
+#define WIFI_SSID    "wifi name"
+#define WIFI_PASS    "wifi pass"
 #define MQTT_BROKER  "iot.cpe.ku.ac.th"
-#define MQTT_USER    "b6510503671"  // ไม่ต้องใส่เครื่องหมาย <>
-#define MQTT_PASS    "panupong.ler@ku.th"      // ไม่ต้องใส่เครื่องหมาย <>
-#define TOPIC_PREFIX "b6510503671"  // ไม่ต้องใส่เครื่องหมาย <>
+#define MQTT_USER    "nontri account name"  // ไม่ต้องใส่เครื่องหมาย <>
+#define MQTT_PASS    "name.name@ku.th"      // ไม่ต้องใส่เครื่องหมาย <>
+#define TOPIC_PREFIX "nontri account name"  // ไม่ต้องใส่เครื่องหมาย <>
