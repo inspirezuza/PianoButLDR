e25c855286951251a543009bed348ebe8ac68b10
inspirezuza <inspire7220@gmail.com>
2023-03-06 12:08:55 +0700

    update iot

--- esp32/sketchpiano/sketchpiano.ino
+++ esp32/sketchpiano/sketchpiano.ino
@@ -1,30 +1,129 @@
-const int notes = 9;
-int ldrPins[notes+1] = {5, 6, 7,15,16,17,18,8,3};
-int ledPins[notes+1] = {42,41,40,42,42,42,42,42,42}; 
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "config.h"
 
-int threshold = 3000;
+
+#define RED_GPIO       42
+#define YELLOW_GPIO    41
+#define GREEN_GPIO     40
+#define LDR_GPIO       4
+#define TOPIC_LIGHT    TOPIC_PREFIX "/light"
+#define TOPIC_LED_RED  TOPIC_PREFIX "/led/red"
+
+const int notes = 9;
+const int max_notes = 12;
+int ldrPins[max_notes+1] = {5, 6, 7,15,16,17,18,8,3};
+int ledPins[max_notes+1] = {42,41,40,42,42,42,42,42,42};
+
+int threshold = 20;
+
+WiFiClient wifiClient;
+PubSubClient mqtt(MQTT_BROKER, 1883, wifiClient);
+uint32_t last_publish;
+
+
+void connect_wifi() {
+  printf("WiFi MAC address is %s\n", WiFi.macAddress().c_str());
+  printf("Connecting to WiFi %s.\n", WIFI_SSID);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    printf(".");
+    fflush(stdout);
+    delay(500);
+  }
+  printf("\nWiFi connected.\n");
+}
+
+
+void connect_mqtt() {
+  printf("Connecting to MQTT broker at %s.\n", MQTT_BROKER);
+  if (!mqtt.connect("", MQTT_USER, MQTT_PASS)) {
+    printf("Failed to connect to MQTT broker.\n");
+    for (;;) {} // wait here forever
+  }
+  mqtt.setCallback(mqtt_callback);
+  mqtt.subscribe(TOPIC_LED_RED);
+  printf("MQTT broker connected.\n");
+}
+
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  if (strcmp(topic, TOPIC_LED_RED) == 0) {
+    payload[length] = 0; // null-terminate the payload to treat it as a string
+    int value = atoi((char*)payload); 
+    if (value == 0) {
+      digitalWrite(RED_GPIO, LOW);
+    }
+    else if (value == 1) {
+      digitalWrite(RED_GPIO, HIGH);
+    }
+    else {
+      printf("Invalid payload received.\n");
+    }
+  }
+  
+  if (strcmp(topic, TOPIC_LED_RED) == 0) {
+    payload[length] = 0; // null-terminate the payload to treat it as a string
+    int value = atoi((char*)payload); 
+    if (value == 0) {
+      digitalWrite(RED_GPIO, LOW);
+    }
+    else if (value == 1) {
+      digitalWrite(RED_GPIO, HIGH);
+    }
+    else {
+      printf("Invalid payload received.\n");
+    }
+  }
+}
+
 
 void setup() {
+  pinMode(RED_GPIO, OUTPUT);
+  digitalWrite(RED_GPIO, 0);
   for (int i = 0; i < notes; i++) {
     pinMode(ldrPins[i], INPUT);
     pinMode(ledPins[i], OUTPUT);
   }
-  // analogSetAttenuation(ADC_11db);
+  connect_wifi();
+  connect_mqtt();
+  last_publish = 0;
   Serial.begin(115200);
 }
 
+
 void loop() {
-  String ldr_string = "";
-  for (int i=0;i<notes;i++){
-    if (analogRead(ldrPins[i]) > threshold){
-      digitalWrite(ledPins[i],1);
-      ldr_string += "1";
+  // check for incoming subscribed topics
+  // mqtt.loop();
+
+
+  // publish light value periodically (without using delay)
+    String ldr_status_mqtt = "";
+    String ldr_py = "";
+    for(int i=0;i<notes;i++){
+      int level = 100 - (analogRead(ldrPins[i])*100/4095);
+      // String payload(level);
+      // printf("Publishing light %d value: %d\n",i, level);
+      ldr_status_mqtt += String(level) + ' ';
+      
+      if (level < threshold){
+        digitalWrite(ledPins[i],1);
+        ldr_py += "1";
+      }
+      else{
+        digitalWrite(ledPins[i],0);
+        ldr_py += "0";
+      }
     }
-    else{
-      digitalWrite(ledPins[i],0);
-      ldr_string += "0";
-    }
+    delay(100);
+    Serial.println(ldr_py);
+    // int level = 100 - (analogRead(LDR_GPIO)*100/4095);
+    // String payload(level);
+    // printf("Publishing light value: %d\n", level);
+    uint32_t now = millis();
+    if (now - last_publish >= 5000) {
+      mqtt.publish(TOPIC_LIGHT, String(ldr_status_mqtt).c_str());
+      last_publish = now;
   }
-  Serial.println(ldr_string);
-  delay(100);
 }
--- main.py
+++ main.py
@@ -10,12 +10,13 @@ PORT = config.PORT
 ser = serial.Serial(PORT , 115200) # open the serial port with the same baud rate as the Arduino code
 
 mixer.init()
-mixer.set_num_channels(15)
+mixer.set_num_channels(50)
 instrument = [filename for filename in os.listdir(r"audio")]
 notes = ['C4','D4','E4','F4','G4','A4','B4','C5','D5']
 
 def playmusic(key,mode):
     # print(key,mode)
+    print(key)
     mixer.Channel(key).play(mixer.Sound(f'audio\\{instrument[mode]}\\{notes[key]}.wav'),fade_ms=100)
     # mixer.Sound(f'audio\\{instrument[mode]}\\{note}.mp3').play(fade_ms=100,)
 
@@ -26,11 +27,12 @@ ldrstatus = [False]*50
 while True:
     data = ser.readline().decode('utf-8').strip() # read a single byte from the serial port
     print(data)
-    for i in range(len(data)):
-        if data[i] == '1' and ldrstatus[i] == False:
-            playmusic(i,mode)
-            ldrstatus[i] = True
-            print(f'play {i}')
-        elif data[i] == '0':
-            ldrstatus[i] = False
-            stopmusic(i)
+    if data[0].isnumeric() and data[-1].isnumeric():
+        for i in range(len(data)):
+            if data[i] == '1' and ldrstatus[i] == False:
+                playmusic(i,mode)
+                ldrstatus[i] = True
+                print(f'play {i}')
+            elif data[i] == '0':
+                ldrstatus[i] = False
+                stopmusic(i)
