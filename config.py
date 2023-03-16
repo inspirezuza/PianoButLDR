07e6ba47449e8be5aca79c78110aa65539352243
inspirezuza <102022496+inspirezuza@users.noreply.github.com>
2023-03-16 15:50:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 PORT = "COM4"
 DEFAULT_INSTRUMENT = "piano"
 MQTT_BROKER = "iot.cpe.ku.ac.th"
-MQTT_USER = "b6510503671" 
-MQTT_PASS = "panupong.ler@ku.th"
-TOPIC_PREFIX = "b6510503671"
+MQTT_USER = "b6510123456" 
+MQTT_PASS = "example.example@ku.th"
+TOPIC_PREFIX = "b6510123456"
 IOT_PORT = 1883
