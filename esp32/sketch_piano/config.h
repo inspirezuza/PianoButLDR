e06400cad6eeb687c4d437545bcf44c560feb656
inspirezuza <102022496+inspirezuza@users.noreply.github.com>
2023-03-16 15:51:01 +0700

    Update config.h

--- esp32/sketch_piano/config.h
+++ esp32/sketch_piano/config.h
@@ -1,6 +1,6 @@
-#define WIFI_SSID    "pichapop"
-#define WIFI_PASS    "12341234"
+#define WIFI_SSID    "wifiname"
+#define WIFI_PASS    "12345678"
 #define MQTT_BROKER  "iot.cpe.ku.ac.th"
-#define MQTT_USER    "b6510503671"  // ไม่ต้องใส่เครื่องหมาย <>
-#define MQTT_PASS    "panupong.ler@ku.th"      // ไม่ต้องใส่เครื่องหมาย <>
-#define TOPIC_PREFIX "b6510503671"  // ไม่ต้องใส่เครื่องหมาย <>
+#define MQTT_USER    "b6510123456"  // ไม่ต้องใส่เครื่องหมาย <>
+#define MQTT_PASS    "example.example@ku.th"      // ไม่ต้องใส่เครื่องหมาย <>
+#define TOPIC_PREFIX "b6510123456"  // ไม่ต้องใส่เครื่องหมาย <>
